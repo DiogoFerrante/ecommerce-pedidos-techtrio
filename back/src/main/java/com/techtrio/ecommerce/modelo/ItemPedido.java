9921f87cc41360954d84a0e8d236b7dc87380f51
BRENO XAVIER DE CASTRO <breno.castro7@senaisp.edu.br>
2026-09-01 22:02:10 -0300

    feat: adiciona classe ItemPedido

---
+++ back/src/main/java/com/techtrio/ecommerce/modelo/ItemPedido.java
@@ -0,0 +1,44 @@
+package com.techtrio.ecommerce;
+
+public class ItemPedido {
+
+    private Produto produto;
+    private int quantidade;
+    private double preco;
+
+    public ItemPedido(Produto produto, int quantidade, double preco) {
+        this.produto = produto;
+        this.quantidade = quantidade;
+        this.preco = preco;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public double calcularSubtotal() {
+        return preco * quantidade;
+    }
+
+    @Override
+    public String toString() {
+        return "ItemPedido{" +
+                "produto=" + produto +
+                ", quantidade=" + quantidade +
+                ", preco=" + preco +
+                ", subtotal=" + calcularSubtotal() +
+                '}';
+    }
+}
