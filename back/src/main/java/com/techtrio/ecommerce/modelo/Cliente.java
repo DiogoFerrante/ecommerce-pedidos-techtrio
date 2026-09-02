9fd48b247e025f6bee9e27695123357f7bb08345
DIOGO ANTONIO FERRANTE <diogo.ferrante@senaisp.edu.br>
2026-09-01 21:47:08 -0300

    feat: adiciona classe Cliente

---
+++ back/src/main/java/com/techtrio/ecommerce/modelo/Cliente.java
@@ -0,0 +1,69 @@
+package com.techtrio.ecommerce.modelo;
+
+public class Cliente {
+
+    private String nome;
+    private String cpf;
+    private String email;
+    private String telefone;
+    private String endereco;
+
+    public Cliente(String nome, String cpf, String email, String telefone, String endereco) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.email = email;
+        this.telefone = telefone;
+        this.endereco = endereco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getIdentificacao() {
+        return nome + " (" + cpf + ")";
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "nome='" + nome + '\'' +
+                ", cpf='" + cpf + '\'' +
+                ", email='" + email + '\'' +
+                ", telefone='" + telefone + '\'' +
+                ", endereco='" + endereco + '\'' +
+                '}';
+    }
+}
